95fa56c52c1d34f453805d64e2c39be96b8a14a5
melissafasol <melissaclairefasol@gmail.com>
2022-10-21 22:35:14 +0100

    debugging 2 recording files

--- run.py
+++ run.py
@@ -11,19 +11,19 @@ from scipy import signal
 from preprocess import ExtractBrainStateEF1ALPHA
 from filter import Filter
 from power import PowerSpectrum, average_power_df
-from ef1_alpha_properties import recordings_1_channels, recordings_2_channels, channels_dict 
+from ef1_alpha_properties import recordings_1_channel, recordings_2_channels, channels_dict 
 
 #paths 
 recording_path = '/home/melissa/PREPROCESSING/EF1_ALPHA'
 brain_state_path = '/home/melissa/PREPROCESSING/EF1_ALPHA/brain_state_folder'
 
 recordings_letters_1 = ['A', 'A_1', 'B', 'C', 'D']
-recordings_letters_2 = ['A', 'A_1', 'B', 'B_1' 'C', 'D']
+recordings_letters_2 = ['A', 'A_1', 'B', 'B_1', 'C', 'D']
 
 one_recording = []
 two_recording = []
 
-brainstates = ['R', 'N'] #W
+brainstates = ['W', 'N', 'R']
 
 
 # for brainstate in brainstates:
@@ -55,18 +55,19 @@ brainstates = ['R', 'N'] #W
 
 for brainstate in brainstates:         
     for headstage_letters in recordings_letters_2:
+        print(headstage_letters)
         for anim_id in recordings_2_channels[headstage_letters]:
-            print(anim_id)
-            print(headstage_letters)
-            preprocessing_steps = ExtractBrainStateEF1ALPHA(anim_id, recording_path, brain_state_path, 
-                                                            headstage_letters, recording_number=2)
+            print('processing data for ' + str(anim_id) + 'for ' + str(brainstate))    
+            preprocessing_steps = ExtractBrainStateEF1ALPHA(anim_id, recording_path, brain_state_path, headstage_letters, recording_number=2)
             part_1, part_2 = preprocessing_steps.load_npy_recordings()
             part_1_br_state, part_2_br_state = preprocessing_steps.load_brain_state_file()
-            if part_1_br_state and part_2_br_state is not None:
-                epoch_indices_1 = preprocessing_steps.remove_E_epochs(part_1_br_state, brain_state_letter=brainstate)
-                epoch_indices_2 = preprocessing_steps.remove_E_epochs(part_2_br_state, brain_state_letter=brainstate)
-                epoch_bins_1 = preprocessing_steps.get_epoch_indices(part_1_br_state, epoch_indices_1)
-                epoch_bins_2 = preprocessing_steps.get_epoch_indices(part_2_br_state, epoch_indices_2)
+            epoch_indices_1 = preprocessing_steps.remove_E_epochs(part_1_br_state, brainstate)
+            epoch_indices_2 = preprocessing_steps.remove_E_epochs(part_2_br_state, brainstate)
+            if epoch_indices_1 and epoch_indices_2 is not None:
+                epoch_1 = preprocessing_steps.get_epoch_indices(epoch_indices_1)
+                epoch_2 = preprocessing_steps.get_epoch_indices(epoch_indices_2)
+                epoch_bins_1 = preprocessing_steps.create_epoch_bins(part_1_br_state, epoch_1)
+                epoch_bins_2 = preprocessing_steps.create_epoch_bins(part_2_br_state, epoch_2)
                 for column, channel in zip(part_1.T, channels_dict[headstage_letters]):
                     filter_steps = Filter(column, epoch_bins_1)
                     filtered_data_1 = filter_steps.butter_bandpass()
@@ -81,7 +82,11 @@ for brainstate in brainstates:
                 dict_data = {'Animal_ID': [anim_id]*len(average_power_array), 'Headstage':[headstage_letters]*len(average_power_array),
                             'Channel': [channel]*len(average_power_array), 'Brainstate': [brainstate]*len(average_power_array),
                             'Power': average_power_array, 'Frequency': frequency_array_part_1}
+                print('data saved for ' + str(anim_id))
                 two_recording.append(pd.DataFrame(data=dict_data))
+            else:
+                pass
     merged_power_file = pd.concat(two_recording, axis = 0).drop_duplicates().reset_index(drop = True)
     os.chdir('/home/melissa/RESULTS/EF1_ALPHA/2_REC')
     merged_power_file.to_csv(str(brainstate) + '_2_rec.csv', index = True)
+    
