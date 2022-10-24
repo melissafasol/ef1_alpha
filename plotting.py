aa28215302c3d19f145ffdd4ec801edb9c54d470
melissafasol <melissaclairefasol@gmail.com>
2022-10-24 22:35:15 +0100

    adding ylim upper and lower bounds

--- plotting.py
+++ plotting.py
@@ -31,5 +31,35 @@ def build_genotype_df(results_dataframe, mutant_list):
     
     return final_dataframe
 
-#separating large df into channels and animal_ids
+#plot average genotype in selective channels 
+
+def plot_geno_average_by_channel(channel_number, color_list, hue_order_list, data_rem, data_nrem, data_wake,
+                                 save_path ):
+    
+    sns.set_style("white") 
+    colors = color_list 
+    hue_order = hue_order_list
+
+    #f.suptitle('REM', fontsize = 36)
+    fig, ax = plt.subplots(1,3, figsize=(20,10), sharex = True, sharey=True)
+    line_rem = sns.lineplot(x= 'Frequency', y='Power', hue='Genotype', hue_order = hue_order, ci= 95, data = data_rem, palette= colors, linewidth = 2, ax = ax[0])
+    ax[0].set(xlabel = 'Frequency (Hz)', ylabel = 'PSD [V**2/Hz]')
+    line_nrem = sns.lineplot(x= 'Frequency', y='Power', hue='Genotype', hue_order = hue_order, ci= 95, data = data_nrem, palette= colors, linewidth = 2, ax = ax[1])
+    ax[1].set(xlabel = 'Frequency (Hz)', ylabel = 'PSD [V**2/Hz]')
+    line_wake = sns.lineplot(x= 'Frequency', y='Power', hue='Genotype', hue_order = hue_order, ci= 95, data = data_wake, palette= colors, linewidth = 2, ax = ax[2])
+    ax[2].set(xlabel = 'Frequency (Hz)', ylabel = 'PSD [V**2/Hz]')
+
+    sns.despine()
+    plt.yscale('log')
+    plt.xlim(1, 100)
+    #plt.ylim(10**-2, 10**)
+
+    plt.suptitle('Channel ' + str(channel_number), fontsize = 'x-large', fontweight = 'bold')
+    line_rem.title.set_text('REM')
+    line_nrem.title.set_text('Non-REM')
+    line_wake.title.set_text(' Wake')
+
+    os.chdir(save_path )
+    plt.savefig('channel_' + str(channel_number) + '_average_genotype.jpg')
+    return plt.plot()
 
